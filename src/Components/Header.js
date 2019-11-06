83f1b81c5f27bb37e562ae4feffada047ad81810
Babatunde <babatundea15@gmail.com>
2019-11-06 13:21:15 +0100

    I created the header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+                <h1 className = '&#178361'>N.A.S.A 🚀 </h1>
+                
+            
+            
+        </div>
+    )
+}
+
+export default Header
